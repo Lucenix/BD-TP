7944c4e8c36a93738c343bd491f789dabcc1d8d1
André Lucena Ferreira <69969391+Lucenix@users.noreply.github.com>
2022-12-28 10:32:12 +0000

    correçao povoamente

--- Scripts/Scrip de Povoamento.sql
+++ Scripts/Scrip de Povoamento.sql
@@ -1,34 +1,55 @@
 Use mydb;
 
+
+delete from TiposConservacao;
+delete from Contacto;
+delete from Relatorio;
+delete from Cliente;
+delete from Percurso;
+delete from Funcionario_has_Percurso;
+delete from DataInspecaoPassada;
+delete from VeiculoTipo;
+delete from Item;
+delete from ItemTipo;
+delete from Compra;
+delete from ItemCompra;
+delete from Endereco;
+delete from Fornecedor;
+delete from EnderecoCliente;
+delete from Funcionario;
+delete from Veiculo;
+
 -- Tipos de Conservacao
-Insert into `TiposConservacao`(`idTiposConservacao`,`Tipo`,`Descrição`)
+Insert into `TiposConservacao`(`idTiposConservacao`,`Tipo`,`Descricao`)
 Values(0,"Temperatura","Conservar à temperatura ambiente");
 
-Insert into `TiposConservacao`(`idTiposConservacao`,`Tipo`,`Descrição`)
-Values(1,"Temperatura","Conservar a uma temperatura abaxio de 0 graus");
+Insert into `TiposConservacao`(`idTiposConservacao`,`Tipo`,`Descricao`)
+Values(1,"Temperatura","Conservar a uma temperatura abaixo de 0 graus");
 
-Insert into `TiposConservacao`(`idTiposConservacao`,`Tipo`,`Descrição`)
+Insert into `TiposConservacao`(`idTiposConservacao`,`Tipo`,`Descricao`)
 Values(2,"Humidade","Manter em ambiente seco");
 
 Select * from TiposConservacao;
 
+
 -- Funcionário
-Insert into `Funcionario`(`idFuncionario`,`Nome`,`Salario`,`HabilitacaoAuto`,`DataEntrada`,`DataExpiracaoHabilitacao`,`Posicao`)
-Values(0,"Bernardo Esteves",5000.0,"BE","2020-03-02","2023-01-19","CEO");
+Insert into `Funcionario`(`idFuncionario`,`Nome`,`Salario`,`HabilitacaoAuto`,`DataEntrada`,`DataExpiracaoHabilitacao`,`Posicao`,`DataNascimento`)
+Values(0,"Bernardo Esteves",5000.0,"BE","2020-03-02","2023-01-19","CEO","1965-08-19");
 
-Insert into `Funcionario`(`idFuncionario`,`Nome`,`Salario`,`HabilitacaoAuto`,`DataEntrada`,`DataExpiracaoHabilitacao`,`Posicao`)
-Values(1,"Júlio Dantas",830.0,"CE","2020-03-02","2025-09-18","Estafeta");
+Insert into `Funcionario`(`idFuncionario`,`Nome`,`Salario`,`HabilitacaoAuto`,`DataEntrada`,`DataExpiracaoHabilitacao`,`Posicao`,`DataNascimento`)
+Values(1,"Júlio Dantas",830.0,"CE","2020-03-02","2025-09-18","Estafeta","1976-05-19");
 
-Insert into `Funcionario`(`idFuncionario`,`Nome`,`Salario`,`HabilitacaoAuto`,`DataEntrada`,`DataExpiracaoHabilitacao`,`Posicao`)
-Values(2,"Almada Negreiros",2000.0,"B1","2020-03-02","2029-01-12","Gestor de Inventário");
+Insert into `Funcionario`(`idFuncionario`,`Nome`,`Salario`,`HabilitacaoAuto`,`DataEntrada`,`DataExpiracaoHabilitacao`,`Posicao`,`DataNascimento`)
+Values(2,"Almada Negreiros",2000.0,"B1","2020-03-02","2029-01-12","Gestor de Inventário","1993-04-07");
 
-Insert into `Funcionario`(`idFuncionario`,`Nome`,`Salario`,`HabilitacaoAuto`,`DataEntrada`,`DataExpiracaoHabilitacao`,`Posicao`)
-Values(3,"Gil Vicente",890.0,"C1E","2020-03-02","2030-05-21","Estafeta");
+Insert into `Funcionario`(`idFuncionario`,`Nome`,`Salario`,`HabilitacaoAuto`,`DataEntrada`,`DataExpiracaoHabilitacao`,`Posicao`,`DataNascimento`)
+Values(3,"Gil Vicente",890.0,"C1E","2020-03-02","2030-05-21","Estafeta","1965-04-03");
 
-Insert into `Funcionario`(`idFuncionario`,`Nome`,`Salario`,`HabilitacaoAuto`,`DataEntrada`,`DataExpiracaoHabilitacao`,`Posicao`)
-Values(4,"Florbela Espanca",900.0,null,"2020-03-02",null,"Técnico Farmacêutico");
+Insert into `Funcionario`(`idFuncionario`,`Nome`,`Salario`,`HabilitacaoAuto`,`DataEntrada`,`DataExpiracaoHabilitacao`,`Posicao`,`DataNascimento`)
+Values(4,"Florbela Espanca",900.0,null,"2020-03-02",null,"Técnico Farmacêutico","1994-12-08");
 
 Select * from Funcionario;
+
 
 -- Contacto (dos funcionários):
 Insert into `Contacto`(`Telemovel`,`Telefone`,`Email`,`Cliente_idCliente`,`Fornecedor_idFornecedor`,`Funcionario_idFuncionario`)
@@ -46,6 +67,7 @@ Values(941110988,252036489,"GilVicente1465@hotmail.com",null,null,3);
 Insert into `Contacto`(`Telemovel`,`Telefone`,`Email`,`Cliente_idCliente`,`Fornecedor_idFornecedor`,`Funcionario_idFuncionario`)
 Values(966345009,null,"FlorBelaLobo@gmail.com",null,null,4);
 
+
 -- Veiculo
 Insert into `Veiculo`(`idVeiculo`,`Categoria`,`Kilometragem`,`TipoCombustivel`,`DataProximaInspecao`,`EstadoOperacional`,`IUC`,`Matricula`)
 Values(0,"CE",12,"Diesel","2023-05-02",1,130.33,"12-AB-34");
@@ -58,12 +80,14 @@ Values(2,"A1",29,"Gasolina","2024-07-29",1,58.31,"20-BD-23");
 
 Select * from Veiculo;
 
+
 -- Relatorio
 Insert into `Relatorio`(`Data`,`EstadoResolucao`,`Descricao`,`Gravidade`,`Funcionario_idFuncionario`,`Veiculo_idVeiculo`)
 Values("2022-11-03 13:23:44",1,"Lateral do carro com alguns riscos","0",3,0);
 
 Insert into `Relatorio`(`Data`,`EstadoResolucao`,`Descricao`,`Gravidade`,`Funcionario_idFuncionario`,`Veiculo_idVeiculo`)
 Values("2022-12-12 11:01:01",1,"Pneu frontal furado","3",1,2);
+
 
 -- Cliente
 Insert into `Cliente`(`idCliente`,`Nome`,`NIF`,`DataNascimento`,`Genero`)
@@ -90,6 +114,7 @@ Values(6,"Guilherme Roques Ribeiro",148035736,null,null);
 Insert into `Cliente`(`idCliente`,`Nome`,`NIF`,`DataNascimento`,`Genero`)
 Values(7,"António José Fernandes Alegre",176315683,"1958-07-03","Masculino");
 
+
 -- Contacto (dos clientes):
 Insert into `Contacto`(`Telemovel`,`Telefone`,`Email`,`Cliente_idCliente`,`Fornecedor_idFornecedor`,`Funcionario_idFuncionario`)
 Values(938795478,null,"JFF32@gmail.com",0,null,null);
@@ -115,6 +140,7 @@ Values(916146021,245129133,"GuilhermeRR@outlook.com",6,null,null);
 Insert into `Contacto`(`Telemovel`,`Telefone`,`Email`,`Cliente_idCliente`,`Fornecedor_idFornecedor`,`Funcionario_idFuncionario`)
 Values(937760678,null,"antonioJoseFA@hotmail.com",7,null,null);
 
+delete from Endereco;
 -- Endereco (dos clientes)
 INSERT INTO `Endereco`(`idEndereco`,`NumeroPorta`, `Rua`, `Localidade`, `CodPostal`, `Fornecedor_idFornecedor`)
 VALUES (0,23,"Rua dos Lagos","Braga","4700-025",null);
@@ -177,25 +203,25 @@ Values(932680618,255512942,"secretaria@brofen.com",null,6,null);
 
 -- Endereco (dos Fornecedores)
 INSERT INTO `Endereco`(`idEndereco`,`NumeroPorta`, `Rua`, `Localidade`, `CodPostal`, `Fornecedor_idFornecedor`)
-VALUES (0,1,"Rua Martha","Famalicão","4760-005",0);
+VALUES (5,1,"Rua Martha","Famalicão","4760-005",0);
 
 INSERT INTO `Endereco`(`idEndereco`,`NumeroPorta`, `Rua`, `Localidade`, `CodPostal`, `Fornecedor_idFornecedor`)
-VALUES (1,122,"Rua Europeia","Braga","4700-030",1);
+VALUES (6,122,"Rua Europeia","Braga","4700-030",1);
 
 INSERT INTO `Endereco`(`idEndereco`,`NumeroPorta`, `Rua`, `Localidade`, `CodPostal`, `Fornecedor_idFornecedor`)
-VALUES (2,100,"Rua Triste","Braga","4700-031",2);
+VALUES (7,100,"Rua Triste","Braga","4700-031",2);
 
 INSERT INTO `Endereco`(`idEndereco`,`NumeroPorta`, `Rua`, `Localidade`, `CodPostal`, `Fornecedor_idFornecedor`)
-VALUES (3,50,"Travessa com tosse","Famalicão","4760-019",3);
+VALUES (8,50,"Travessa com tosse","Famalicão","4760-019",3);
 
 INSERT INTO `Endereco`(`idEndereco`,`NumeroPorta`, `Rua`, `Localidade`, `CodPostal`, `Fornecedor_idFornecedor`)
-VALUES (4,32,"Rua Feliz","Braga","4700-004",4);
+VALUES (9,32,"Rua Feliz","Braga","4700-004",4);
 
 INSERT INTO `Endereco`(`idEndereco`,`NumeroPorta`, `Rua`, `Localidade`, `CodPostal`, `Fornecedor_idFornecedor`)
-VALUES (5,21,"Rua Omni","Guimarães","4760-025",5);
+VALUES (10,21,"Rua Omni","Guimarães","4760-025",5);
 
 INSERT INTO `Endereco`(`idEndereco`,`NumeroPorta`, `Rua`, `Localidade`, `CodPostal`, `Fornecedor_idFornecedor`)
-VALUES (6,190,"Rua BigBru","Famalicão","4760-026",6);
+VALUES (11,190,"Rua BigBru","Famalicão","4760-026",6);
 
 -- Item
 Insert into `Item`(`idItem`,`Nome`,`Imposto`,`Descricao`,`Custo`,`Quantidade`,`Comparticipacao`)
@@ -227,28 +253,28 @@ Values(8,"WHEY",0.23,"Proteína em pó, isolada, 1kg",65.99,2,0);
 
 -- Compras
 INSERT INTO `Compra`(`idCompra`, `CustoTotal`, `DataEmissao`, `DataEntrega`, `Fornecedor_idFornecedor`)
-VALUES (0, "2018-12-21 08:32:12", "2019-1-13 16:05:50", 0);
+VALUES (0, 0, "2018-12-21 08:32:12", "2019-1-13 16:05:50", 0);
 
 INSERT INTO `Compra`(`idCompra`, `CustoTotal`, `DataEmissao`, `DataEntrega`, `Fornecedor_idFornecedor`)
-VALUES (1, "2020-05-07 16:30:04", "2012-5-23 15:56:03", 1);
+VALUES (1, 0, "2020-05-07 16:30:04", "2012-5-23 15:56:03", 1);
 
 INSERT INTO `Compra`(`idCompra`, `CustoTotal`, `DataEmissao`, `DataEntrega`, `Fornecedor_idFornecedor`)
-VALUES (2, "2009-09-30 10:00:01", "2009-10-5 11:45:01", 2);
+VALUES (2, 0, "2009-09-30 10:00:01", "2009-10-5 11:45:01", 2);
 
 INSERT INTO `Compra`(`idCompra`, `CustoTotal`, `DataEmissao`, `DataEntrega`, `Fornecedor_idFornecedor`)
-VALUES (3, "2002-09-20 17:30:00", "2002-11-01 07:00:30", 3);
+VALUES (3, 0, "2002-09-20 17:30:00", "2002-11-01 07:00:30", 3);
 
 INSERT INTO `Compra`(`idCompra`, `CustoTotal`, `DataEmissao`, `DataEntrega`, `Fornecedor_idFornecedor`)
-VALUES (4, "2018-06-14 15:15:15", "2018-06-17 16:03:20", 4);
+VALUES (4, 0, "2018-06-14 15:15:15", "2018-06-17 16:03:20", 4);
 
 INSERT INTO `Compra`(`idCompra`, `CustoTotal`, `DataEmissao`, `DataEntrega`, `Fornecedor_idFornecedor`)
-VALUES (5, "2019-07-20 05:59:50", "2019-07-29 19:30:30", 5);
+VALUES (5, 0, "2019-07-20 05:59:50", "2019-07-29 19:30:30", 5);
 
 INSERT INTO `Compra`(`idCompra`, `CustoTotal`, `DataEmissao`, `DataEntrega`, `Fornecedor_idFornecedor`)
-VALUES (6, "2021-02-17 20:00:40", "2021-02-29 19:35:35", 6);
+VALUES (6,0, "2021-02-17 20:00:40", "2021-02-29 19:35:35", 6);
 
 INSERT INTO `Compra`(`idCompra`, `CustoTotal`, `DataEmissao`, `DataEntrega`, `Fornecedor_idFornecedor`)
-VALUES (7, "2017-05-04 12:04:06", "2017-05-20 14:27:50", 4);
+VALUES (7,0, "2017-05-04 12:04:06", "2017-05-20 14:27:50", 4);
 
 -- ItemCompra
 INSERT INTO `ItemCompra`(`PrazoDevalidade`, `CustoParcial`, `Quantidade`, `Item_iditem`, `Compra_idCompra`)
