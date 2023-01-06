f7c565fbfa59ec85f1d7385f2d34508bb854e0b5
André Lucena Ferreira <69969391+Lucenix@users.noreply.github.com>
2023-01-06 15:22:53 +0000

    failed query

--- Scripts/Query_Script.sql
+++ Scripts/Query_Script.sql
@@ -1,6 +1,4 @@
 use mydb;
-select * from cliente;
-
 
 -- Visualizar todos os percursos de um dado funcionário 
 select f.Nome,fp.percurso_idpercurso as NrPercurso from (funcionario as f 
@@ -13,3 +11,16 @@ select r.descricao,r.gravidade,v.idVeiculo,v.Matricula from relatorio as r
 	where r.veiculo_idveiculo = 2 or r.Veiculo_idVeiculo = "20-BD-23";
     
 -- Deve ser possível verificar quanto tempo uma Encomenda esteve em trânsito
+select datediff(e.horaenvio,e.horaentrega) from encomenda as e
+		where e.idEncomenda = 1;
+
+drop procedure if exists TempoTransitoEncomenda;
+delimiter $$
+create procedure TempoTransitoEncomenda(in idEncomenda int, out diff datetime)
+	begin
+	select datediff(e.horaenvio,e.horaentrega) into diff from encomenda as e
+		where e.idEncomenda = idEncomenda;
+	end; $$
+
+call TempoTransitoEncomenda(1, @diff);
+select @diff as diff;
